65518d20777e34683ac30958fc960b4f890a5baa
omaradil28 <163222841+omaradil28@users.noreply.github.com>
2024-04-09 14:26:57 -0700

    Create PA7.hpp

---
+++ PA7.hpp
@@ -0,0 +1,16 @@
+#pragma once
+#define _CRT_SECURE_NO_WARNINGS
+#define SIZEOFBUFFER 50
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <cctype>
+#include <sstream>
+#include <cstring>
+
+using std::fstream;
+using std::cout;
+using std::endl;
+using std::cin;
